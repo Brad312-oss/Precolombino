b9db54265aa10396fd584eb9681b71e7655b427f
Brad312-oss <brolymaster312@gmail.com>
2025-07-25 15:07:10 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,10 @@ import mysql from 'mysql2/promise';
 // Creamos un "pool" (conjunto) de conexiones a la base de datos.
 // Un pool permite gestionar múltiples conexiones de manera eficiente.
 export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'sena',
-    database: 'precolombinos',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
 });
