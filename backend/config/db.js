8fc1015e4df9fcc31ada7e3bed693522bbaf236e
Brad312-oss <brolymaster312@gmail.com>
2025-09-30 05:24:39 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,7 @@
 // Esto facilita el uso de async/await al trabajar con la base de datos.
 import mysql from 'mysql2/promise';
 
-// Creamos un "pool" (conjunto) de conexiones a la base de datos.
-// Un pool permite gestionar múltiples conexiones de manera eficiente.
-export const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
+//
     connectionLimit: 10,
 });
 
